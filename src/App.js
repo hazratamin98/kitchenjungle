b0cdb9899cad9de9a5cc61eaf1c0a1473e8133c5
hazratamin98 <hazratamin98@gmail.com>
2022-03-17 16:32:36 +0500

    title rename

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import "./App.css"
-import Navbar from "./Components/Navbar/Navbar"
-import Footer from "./Components/Footer/Footer"
+import "./App.css";
+import React, { useEffect } from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
 
-import NewsLetter from "./Components/NewsLetter/NewsLetter"
+import NewsLetter from "./Components/NewsLetter/NewsLetter";
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 function App() {
+  useEffect(() => {
+    document.title = "Kitchen Jungle";
+  }, []);
   return (
     <div>
       <Navbar />
       <NewsLetter />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
