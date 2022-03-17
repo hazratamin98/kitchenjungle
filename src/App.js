02bda879bbfe204a4bf49edf051b21d3074b76c9
hazratamin98 <hazratamin98@gmail.com>
2022-03-17 15:42:08 +0500

    log in

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import "./App.css"
 import Navbar from "./Components/Navbar/Navbar"
 import Footer from "./Components/Footer/Footer"
-import Hero from "./Components/Hero/Hero"
-import Login from "./Components/Login/Login"
+
 import NewsLetter from "./Components/NewsLetter/NewsLetter"
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
