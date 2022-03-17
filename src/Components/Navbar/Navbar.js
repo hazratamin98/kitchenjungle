a7a30a961948cffcf3e2181a5fe0997057c0763c
hazratamin98 <hazratamin98@gmail.com>
2022-03-17 12:58:49 +0500

    user Icon

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 
 const Navbar = () => {
   return (
@@ -80,12 +80,26 @@ const Navbar = () => {
                   CONTACT
                 </a>
               </li>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                className="h-5 w-5"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M16 7a4 4 0 11-8 0 4 4 0 018 0zm-4 7a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                ></path>
+              </svg>
             </ul>
           </div>
         </div>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
