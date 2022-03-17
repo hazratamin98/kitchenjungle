7eb61fcfb80149416b330d02d5b480d918eaaf91
hazratamin98 <hazratamin98@gmail.com>
2022-03-17 12:54:15 +0500

    Footer setup

---
+++ src/Components/Hero/Hero.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Hero = () => {
+  return (
+    <div>
+      <h1 className="bg-red-200">Hello World</h1>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,91 @@
+import React from "react"
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 ">
+        <div className="container flex flex-wrap justify-between items-center mx-auto">
+          <a href="#" class="flex items-center">
+            {/* <img src="/docs/images/logo.svg" class="mr-3 h-6 sm:h-10" alt="Flowbite Logo"> */}
+            <span className="self-center text-xl ml-12 font-semibold whitespace-nowrap ">
+              Kitchen Jungle
+            </span>
+          </a>
+          <div className="flex items-center md:order-2">
+            <button
+              data-collapse-toggle="mobile-menu-2"
+              type="button"
+              class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="mobile-menu-2"
+              aria-expanded="false"
+            >
+              <span class="sr-only">Open main menu</span>
+              <svg
+                className="w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </button>
+          </div>
+          <div
+            className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1"
+            id="mobile-menu-2"
+          >
+            <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 "
+                  aria-current="page"
+                >
+                  HOME
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
+                >
+                  RECIPES
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0  md:dark:hover:text-white  md:dark:hover:bg-transparent "
+                >
+                  SUBMIT RECIPE
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0  md:dark:hover:text-white   md:dark:hover:bg-transparent "
+                >
+                  FAVORITES
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0  md:dark:hover:text-white  md:dark:hover:bg-transparent "
+                >
+                  CONTACT
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
