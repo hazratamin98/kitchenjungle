280b9bcb46719ed2df0040db26fb0226ff337292
hazratamin98 <hazratamin98@gmail.com>
2022-03-17 16:00:30 +0500

    href updated

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -19,9 +19,7 @@ const index = () => {
           </h2>
           <ul className="text-gray-300">
             <li className="mb-4">
-              <a href="#" className="hover:underline">
-                HOME
-              </a>
+              <a className="hover:underline">HOME</a>
             </li>
             <li className="mb-4">
               <a href="resipes" className="hover:underline">
@@ -29,22 +27,16 @@ const index = () => {
               </a>
             </li>
             <li className="mb-4">
-              <a href="Submit" className="hover:underline">
-                Submit Recipe
-              </a>
+              <a className="hover:underline">Submit Recipe</a>
             </li>
-            <a href="CONTACT US" className="hover:underline"></a>
+            <a className="hover:underline"></a>
             <li className="mb-4">
-              <a href="CONTACT US" className="hover:underline"></a>
-              <a href="CONTACT US" className="hover:underline">
-                CONTACT US
-              </a>
+              <a className="hover:underline"></a>
+              <a className="hover:underline">CONTACT US</a>
             </li>
             <li className="mb-4">
-              <a href="#" className="hover:underline"></a>
-              <a href="#" className="hover:underline">
-                ABOUT US
-              </a>
+              <a className="hover:underline"></a>
+              <a className="hover:underline">ABOUT US</a>
             </li>
           </ul>
         </div>
@@ -54,19 +46,13 @@ const index = () => {
           </h2>
           <ul className="text-gray-300">
             <li className="mb-4">
-              <a href="FAQs" className="hover:underline">
-                FAQs
-              </a>
+              <a className="hover:underline">FAQs</a>
             </li>
             <li className="mb-4">
-              <a href="#" className="hover:underline">
-                Terms &amp; Conditions
-              </a>
+              <a className="hover:underline">Terms &amp; Conditions</a>
             </li>
             <li className="mb-4">
-              <a href="#" className="hover:underline">
-                Privacy Policy
-              </a>
+              <a className="hover:underline">Privacy Policy</a>
             </li>
           </ul>
         </div>
@@ -114,9 +100,7 @@ const index = () => {
                   d="M16 8l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2M5 3a2 2 0 00-2 2v1c0 8.284 6.716 15 15 15h1a2 2 0 002-2v-3.28a1 1 0 00-.684-.948l-4.493-1.498a1 1 0 00-1.21.502l-1.13 2.257a11.042 11.042 0 01-5.516-5.517l2.257-1.128a1 1 0 00.502-1.21L9.228 3.683A1 1 0 008.279 3H5z"
                 ></path>
               </svg>
-              <a href="#" className="hover:underline">
-                +2 333 123 4567
-              </a>
+              <a className="hover:underline">+2 333 123 4567</a>
             </li>
             <li className="mb-4  flex  items-center gap-4 justify-center">
               <svg
@@ -133,9 +117,7 @@ const index = () => {
                   d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                 ></path>
               </svg>
-              <a href="#" className="hover:underline">
-                info@yourdomain.com
-              </a>
+              <a className="hover:underline">info@yourdomain.com</a>
             </li>
           </ul>
         </div>
@@ -160,7 +142,7 @@ const index = () => {
               ></path>
             </svg>
           </a>
-          <a href="#" className="text-gray-400 hover:text-white">
+          <a className="text-gray-400 hover:text-white">
             <svg
               className="w-5 h-5"
               fill="currentColor"
@@ -174,7 +156,7 @@ const index = () => {
               ></path>
             </svg>
           </a>
-          <a href="#" className="text-gray-400 hover:text-white">
+          <a className="text-gray-400 hover:text-white">
             <svg
               className="w-5 h-5"
               fill="currentColor"
@@ -184,7 +166,7 @@ const index = () => {
               <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path>
             </svg>
           </a>
-          <a href="#" className="text-gray-400 hover:text-white">
+          <a className="text-gray-400 hover:text-white">
             <svg
               className="w-5 h-5"
               fill="currentColor"
@@ -198,7 +180,7 @@ const index = () => {
               ></path>
             </svg>
           </a>
-          <a href="#" className="text-gray-400 hover:text-white">
+          <a className="text-gray-400 hover:text-white">
             <svg
               className="w-5 h-5"
               fill="currentColor"
