915594d86e0aa71e309d0bd356a7ca5939d06c20
hazratamin98 <hazratamin98@gmail.com>
2022-03-22 14:56:35 +0500

    context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -4,5 +4,6 @@ const AuthContext = createContext()
 
 export const AuthProvider = ({ children }) => {
   return <AuthContext.Provider value={""}>{children}</AuthContext.Provider>
+}
 
 export default AuthContext
